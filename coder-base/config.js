a6ad3392117d3659768e0dd53428073edcbc798b
Cap123 <williamepj4@gmail.com>
2013-12-07 07:09:32 +0000

    Update config.js

--- coder-base/config.js
+++ coder-base/config.js
@@ -1,4 +1,3 @@
-
 exports.listenIP = null; //Defaults to *
 exports.listenPort = '8081'; //the SSL port things run on
 exports.httpListenPort = '8080'; //this will all be redirected to SSL
@@ -11,7 +10,7 @@ exports.httpsVisiblePort = '443'; //forwarded https port the user sees
 exports.country = "US";
 exports.state = "New York";
 exports.locale = "New York";
-exports.commonName = "coder.local";
+exports.commonName = "captechz.local";
 exports.subjectAltName = "DNS:192.168.0.1";
 
 
